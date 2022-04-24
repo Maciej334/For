b4efd16bd2305491c11d4b4d3d73cd9916359cf4
Maciej334 <104215335+Maciej334@users.noreply.github.com>
2022-04-24 11:05:57 +0200

    A liitle bit working

---
+++ for.py
@@ -0,0 +1,17 @@
+#for fruit in fruits:
+    #if fruit == "banana":
+        #break
+    #print(fruit)
+
+#for fruit in fruits:
+    #if fruit == "banana":
+       # break
+    #print(fruit)
+
+#for x in range(2, 10):
+    #print(x)
+
+for x in range(5):
+    for y in range(5):
+        for z in range(5):
+            print("%s,%s,%s" % (x,y,z))
